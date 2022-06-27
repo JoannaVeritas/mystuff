ba42243c1a416524bc660888eebc429a4aeb0045
Joanna <joan.e.bednarska@gmail.com>
2022-06-27 14:09:59 +0100

    poprawiony bug, gdzie wywalal blad przy podaniu litery z konca alpha

--- ex_Marty/caesarcipher.py
+++ ex_Marty/caesarcipher.py
@@ -4,18 +4,20 @@
 
 def cipher(message):
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'q', 'x', 'y', 'z']
-    alpha = ''.join(alphabet)
+    alph = ''.join(alphabet)
+    alpha = alph + alph
+    print(alpha)
     encrypted = ''
     shift = int(input('provide shift value: '))
     if shift < 2*len(alphabet):
-        alphabet = alphabet + alphabet + alphabet
+        for letter in message:
+            pos = alpha.find(letter)
+            nlet = alpha[pos+shift]
+            encrypted += nlet
     else:
         print('Przesuniecie to big')
         quit()
-    for letter in message:
-        pos = alpha.find(letter)
-        nlet = alpha[pos+shift]
-        encrypted += nlet
+
     return encrypted
 
 
